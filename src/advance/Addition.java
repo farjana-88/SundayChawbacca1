f66133a9cd28ec13e58e7278cea356b54c6a154e
farjana-88 <farjana@Farjanas-MBP.home>
2022-09-25 13:15:03 -0400

    New class

---
+++ src/advance/Addition.java
@@ -0,0 +1,48 @@
+package advance;
+
+public class Addition {
+int x;
+int y;
+void add() {
+	int sum;
+	sum=x+y;
+	System.out.println(sum);
+}
+
+int add(int a,int b,int c) {
+	int sum=a+b+c;
+	return sum;
+}
+
+ void add(double e,double f,double g) {
+	 double sum=e+f+g;
+	 System.out.println(sum);
+ }
+
+
+
+public static void main(String[] args) {
+	
+	Addition  value = new Addition() ;
+	value.x=10;
+	value.y=5;
+	value.add();
+	int result= value.add(5, 10, 50);
+	System.out.println(result);
+	
+	value.add(2.5, 3.6, 2.8);
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+}
---
+++ src/advance/Subtraction.java
@@ -0,0 +1,27 @@
+package advance;
+
+public class Subtraction extends Addition{
+
+	
+	void sub (int a, int b) {
+		int s = a-b;
+		System.out.println(s);
+		
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	public static void main(String[] args) {
+	Subtraction s1 = new Subtraction();
+	
+	s1.sub(100,25);
+	System.out.println(s1.add(10, 10, 10));
+
+	}
+
+}
